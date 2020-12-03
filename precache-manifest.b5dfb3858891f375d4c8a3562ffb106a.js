13392d556fa1eecf6a6352c9b33760fdcf7445b2
Sumod Sivadas <contactsumod@gmail.com>
2020-12-03 14:53:50 +0530

    Updates

---
+++ precache-manifest.b5dfb3858891f375d4c8a3562ffb106a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a70136edae8adf95df63ec56b29c65",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "2c2ba54e984e811a0f22",
+    "url": "/random-quotes/static/css/main.d78c4c0f.chunk.css"
+  },
+  {
+    "revision": "815879a68a7ff23476dc",
+    "url": "/random-quotes/static/js/2.1071f18e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/random-quotes/static/js/2.1071f18e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2ba54e984e811a0f22",
+    "url": "/random-quotes/static/js/main.895176f7.chunk.js"
+  },
+  {
+    "revision": "973348987803fdb311dd",
+    "url": "/random-quotes/static/js/runtime-main.e3b6bf3a.js"
+  },
+  {
+    "revision": "a0f3e2f9a1724b929c89649ed115c208",
+    "url": "/random-quotes/static/media/art.a0f3e2f9.svg"
+  },
+  {
+    "revision": "473c98fe9850e4a350bee6a71bc483e7",
+    "url": "/random-quotes/static/media/imagination.473c98fe.svg"
+  },
+  {
+    "revision": "8b09de3dd0c5cfc55c4ba656bf0db4bb",
+    "url": "/random-quotes/static/media/inspire.8b09de3d.svg"
+  },
+  {
+    "revision": "88556646f36a87deb50fa62baaa19bf6",
+    "url": "/random-quotes/static/media/mobiledev.88556646.svg"
+  },
+  {
+    "revision": "06b9308079aabea3403c98f638d86271",
+    "url": "/random-quotes/static/media/omega.06b93080.svg"
+  },
+  {
+    "revision": "2c3054c1419cb42581accf92387d8412",
+    "url": "/random-quotes/static/media/opinions.2c3054c1.svg"
+  },
+  {
+    "revision": "41f8b740a193c87883c9db9731005102",
+    "url": "/random-quotes/static/media/people.41f8b740.svg"
+  },
+  {
+    "revision": "ab129dd2f0a3e621dd6a6d82eca69ee2",
+    "url": "/random-quotes/static/media/road.ab129dd2.svg"
+  },
+  {
+    "revision": "97968f99ed853106106cb68d4f074cce",
+    "url": "/random-quotes/static/media/socials.97968f99.svg"
+  },
+  {
+    "revision": "a96d368a8c56c89785602902d5f6811d",
+    "url": "/random-quotes/static/media/tea.a96d368a.svg"
+  },
+  {
+    "revision": "6e2942332f31ea52d48d5a35f56eca5c",
+    "url": "/random-quotes/static/media/time.6e294233.svg"
+  }
+]);
